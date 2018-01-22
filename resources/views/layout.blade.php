7e141c6aaa616ec96d9e832e14a8df7767b23fc5
longton95 <longton95@gmail.com>
2018-01-22 17:37:26 +0000

    Starting layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,16 @@
    <title></title>
 </head>
 <body>
+<div class="container">
+   <div class="alert alert-primary" role="alert">
+     This is a primary alert—check it out!
+   </div>
 
-   @yield
+   @yield('content')
+
+</div>
+
+   @yield('footer')
 
 </body>
 </html>
