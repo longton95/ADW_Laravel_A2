83ecc25d4f35c3f780e9770b2c5d83c605ca4677
longton95 <longton95@gmail.com>
2018-01-28 18:50:57 +0000

    Made fields protected in the wallet model

--- app/Wallet.php
+++ app/Wallet.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
+   protected $fillable = ['name', 'bitcoin', 'etherium', 'litecoin'];
+
    public function user(){
 
       return $this->belongsTo(User::class);
