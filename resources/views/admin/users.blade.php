96e1dcd3ee0dcc4dd1d3ac14be93ae6505515f50
longton95 <longton95@gmail.com>
2018-01-29 20:00:46 +0000

    changed to admin user page

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-   <title></title>
-</head>
-<body>
-   
-   <h1>{{ $user->firstName }}</h1>
+@extends('layouts.app')
 
-</body>
-</html>
+@section('content')
+<div class="container">
+   <div class="row mt-5">
+      <div class="col-md-10 offset-md-1">
+         <div class="card">
+            <div class="card-header">Dashboard for {{ Auth::user()->firstName }} {{ Auth::user()->lastName }}</div>
+
+            <div class="card-body">
+               <h1>{{ $user->firstName }}</h1>
+            </div>
+
+         </div>
+      </div>
+   </div>
+</div>
+@endsection
