b911bc956562380a1014d3fff29c5171ad7e9ff7
longton95 <longton95@gmail.com>
2018-01-29 19:54:15 +0000

    Added groups to the routes

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,25 @@
 
 Auth::routes();
 
-Route::get('/', 'adminController@index');
+Route::get('/', 'userController@index')->name('index');
 
-Route::get('/admin/{user}', 'adminController@details');
+Route::get('wallet', 'userController@index')->name('wallet');
 
-Route::get('/wallet', 'userController@index');
+Route::get('createWallet', 'userController@createWallet')->name('createWallet');
 
-Route::get('/createWallet', 'userController@createWallet');
+Route::get('editWallet', 'userController@editWallet')->name('editWallet');
 
-Route::get('/editWallet', 'userController@editWallet');
+Route::patch('wallet/{id}', 'userController@update');
 
-Route::patch('/wallet/{id}', 'userController@update');
+Route::post('createWallet', 'userController@store');
 
 
+Route::group(['middleware' => 'admin'], function()
+{
+   Route::get('admin', 'adminController@index')->name('admin');
+
+   Route::get('admin/{user}', 'adminController@details')->name('details');
+
+   Route::get('admin/{user}/delete', 'adminController@delete')->name('deleteUser');
+});
 
