48867bcfca36668022a6582ff4285cfed63ceebb
longton95 <longton95@gmail.com>
2018-02-02 20:55:12 +0000

    Update formatting adminController

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use App\User;
 use Auth;
 
-
 class adminController extends Controller
 {
     /**
@@ -15,33 +14,32 @@ class adminController extends Controller
     *
     * @return void
     */
-    public function __construct() {
-
+    public function __construct()
+    {
         $this->middleware('auth');
     }
 
-    public function index() {
-
+    public function index()
+    {
         $users = User::with('wallets')->get();
 
         return view('admin.index', compact('users'));
     }
 
 
-    public function details($id) {
+    public function details($id)
+    {
+        $user = User::find($id);
 
-      $user = User::find($id);
+        $wallets = User::find($id)->wallets;
 
-      $wallets = User::find($id)->wallets;
-
-        return view('admin.users', compact('user','wallets'));
+        return view('admin.users', compact('user', 'wallets'));
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
+        User::destroy($id);
 
-      User::destroy($id);
-
-      return redirect('/admin');
-
+        return redirect('/admin');
     }
 }
