4cc8be408a944f1b1ca2e2a91002d83c6d4f127d
longton95 <longton95@gmail.com>
2018-03-16 11:11:43 +0000

    added database changes migration changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+use Jenssegers\Mongodb\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('email')->unique();
-            $table->string('role')->default('user');
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('users', function (Blueprint $collection) {
+            $collection->increments('id');
+            $collection->string('firstName');
+            $collection->string('lastName');
+            $collection->string('email')->unique();
+            $collection->string('role')->default('user');
+            $collection->string('password');
+            $collection->rememberToken();
+            $collection->timestamps();
         });
     }
 
