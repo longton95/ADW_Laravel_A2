388aaf2fbde862e4694935ed9cc8f954774b7ebc
longton95 <longton95@gmail.com>
2018-04-15 18:44:32 +0100

    Updated to allow multiple Oauth providors

--- config/services.php
+++ config/services.php
@@ -34,11 +34,29 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
-    
+
     'google' => [
        'client_id'     => env('GOOGLE_ID'),
        'client_secret' => env('GOOGLE_SECRET'),
        'redirect'      => env('GOOGLE_REDIRECT')
-   ]
+   ],
+
+   'github' => [
+      'client_id'     => env('GITHUB_ID'),
+      'client_secret' => env('GITHUB_SECRET'),
+      'redirect'      => env('GITHUB_REDIRECT')
+  ],
+
+  'facebook' => [
+     'client_id'     => env('FACEBOOK_ID'),
+     'client_secret' => env('FACEBOOK_SECRET'),
+     'redirect'      => env('FACEBOOK_REDIRECT')
+  ],
+
+  'twitter' => [
+     'client_id'     => env('TWITTER_ID'),
+     'client_secret' => env('TWITTER_SECRET'),
+     'redirect'      => env('TWITTER_REDIRECT')
+  ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,26 @@
 Auth::routes();
 
 Route::get('/', 'userController@index')->name('index');
-
-Route::get('login/google', 'Auth\LoginController@redirectToProvider');
-Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
-
 Route::get('wallet', 'userController@Wallets')->name('wallet');
-
 Route::get('createWallet', 'userController@createWallet')->name('createWallet');
-
 Route::get('editWallet', 'userController@editWallet')->name('editWallet');
+Route::get('deleteWallet', 'userController@deleteWallet')->name('deleteWallet');
+
+
 
 Route::patch('wallet/{id}', 'userController@update');
-
+Route::get('wallet/delete/{id}', 'userController@delete');
 Route::post('createWallet', 'userController@store');
+
+
+Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 
 
 Route::group(['middleware' => 'admin'], function()
 {
    Route::get('admin', 'adminController@index')->name('admin');
-
    Route::get('admin/{user}', 'adminController@details')->name('details');
-
    Route::get('admin/delete/{user}', 'adminController@delete')->name('deleteUser');
 });
 
