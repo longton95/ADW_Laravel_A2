b20350be017296c1b49470c838966cb8457b57eb
longton95 <longton95@gmail.com>
2018-04-27 21:57:34 +0100

    added form aliases

--- config/app.php
+++ config/app.php
@@ -232,6 +232,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
         'Moloquent' => 'Jenssegers\Mongodb\Eloquent\Model',
 
     ],
